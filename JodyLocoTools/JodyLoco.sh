ffa5a9eafdcd9161d965cc477c10fe2a58158190
SiapaAku666 <42209636+SiapaAku666@users.noreply.github.com>
2018-08-08 20:22:52 +0700

    Add files via upload

---
+++ JodyLocoTools/JodyLoco.sh
@@ -0,0 +1,201 @@
+#NgalocoSambilKetawa
+#NgeweDiToilet
+#Hacking
+#IndonesianCyberCrime
+#IndonesianCyberCrime
+clear
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay "T00Ls" | lolcat
+toilet -f big -F gay "JodyLoco" 
+echo ""      
+echo "\033[32;1mAuthor : Jody Lucu Kalau Lagi Sange" 
+echo "\033[35;1mTeam : Jody lagi sange"
+echo "\033[33;1mKontak Me :  Teu boga kontak aing mah anjing"
+echo "\033[31;1mSubcribe Youtube: JodySange"
+echo "\033[33;1mDarkness Cyber Team" "\033[32;1m&" "\033[36;1mIndonesia Security Lite"
+echo "\033[34;1m[============================[>"
+sleep 1
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake Para ngentot ngentot anjing:)?:"
+echo "\033[36;1m"
+echo "[===============================================[>"
+echo $green "1.> Tool Pengirim Bokep"
+echo "[===============================================]>"
+echo $purple "2.> Tools Mengirim sms mengajak ewean"
+echo "[===============================================]>"
+echo $cyan "3.> Tools mengirim sms Jody Lagi sange mengajak Ewean kepada para memek memek"
+echo "[===============================================]>"
+echo $red "4.> Tools Memek Berdarah untuk menyerang Android"
+echo "[===============================================]>"
+echo $white "5.> Tools SPAM jody dan Tante Lux"
+echo "[===============================================]>"
+echo $green "6.> Spam Sms Jody Minta Tante Lux"
+echo "[===============================================]>"
+echo $purple "7.> Spam Sms Whiskas Kucing lagi sakit kanjut"
+echo "[===============================================]>"
+echo $cyan "8.> Spam Sms ZIPZIPZIP anjing"
+echo "[===============================================]>"
+echo $red "9.> Jangan di buka nanti Malah Enak"
+echo "[===============================================]>"
+echo $white "10.> Spam Akun Gmail orang lain yuk anjing"
+echo "[===============================================]>"
+echo $green "11.> Whois LookUp"
+echo "[===============================================]>"
+echo $purple "12.> Upload Admin Finder"
+echo "[===============================================]>"
+echo $cyan "13.> Script Deface Creator"
+echo "[===============================================]>"
+echo $red "14.> ReConDog Buat Information gathering"
+echo "[===============================================]>"
+echo $white "15.> Admin Login Finder"
+echo "\033[32;1m"
+read -p "[root@T0OLs-JodyLoco:~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "JodyLoco" | lolcat
+cd module
+php ngentot.php
+fi
+
+if
+[ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd module
+php t.php
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+figlet "Mr.B4J1N64N"
+cd module
+php mataharimall.php
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd module
+php hooq.php
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+toilet -f slant --gay "Mr.B4J1N64N"
+cd module
+php kfc.php
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet -f mono12 -F gay "Mr.B4J1N64N"
+cd module
+php phd.php
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd module
+php whiskas.php
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+figlet "Mr.B4J1N64N"
+cd module
+php zipay.php
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd module
+php key.php
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd module
+python2 SpamMail.py
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd module
+python2 whs.py
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd module
+python2 upf.py
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd module
+python2 create.py
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd module
+python2 dog.py
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd module
+python2 gam.py
+fi
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "\033[32;1mDARKNESS CYBER TEAM"
+sleep 1
+echo "\033[33;1mWe Security"
+sleep 1
+echo " We Not Friends"
+sleep 1
+echo "We Are Family"
+sleep 1
+echo "Hacking Is Not Criminal;)"
+sleep 1
+echo "Ketika Sebuah Hayalan Tidak tercapai"
+sleep 1
+echo "Maka Terus lah BerJuang Dan Berusaha:)"
+sleep 1
+echo "\033[32;1mKarna Suatu Hari Nanti Kamu akan Mendapatkannya:)"
+sleep 1
+exit
+fi
+
---
+++ JodyLocoTools/ReadMe.md
@@ -0,0 +1,8 @@
+SEMUA LO BAJINGAN !!!!
+Android dan Komputer lo udah gua pasang BackPintu :v wkwkwkwkwkw
+terima kasih banget anjingg para hacker indonesia sudah download zip ane ANJING !!!!!!!
+
+ANDA INGIN TAU SIAPA SAYA SEBENERNYA ???????
+ PANGGIL SAYA "Tanpa Nama Belakang"
+ 
+ MAMPUSSSS ANJING :v ckckckkckcck
